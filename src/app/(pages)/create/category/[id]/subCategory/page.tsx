28be78ae337b2eeb4763614d8e43d1732bd6daf4
vaxad <varadprabhu111@gmail.com>
2024-07-22 00:29:59 +0530

    fix: build issues

--- src/app/(pages)/create/category/[id]/subCategory/page.tsx
+++ src/app/(pages)/create/category/[id]/subCategory/page.tsx
@@ -2,6 +2,7 @@
 export default function Page() {
     return (
         <div>
+            page
         </div>
     )
 }
--- src/app/(pages)/create/category/[id]/subCategory/resource/page.tsx
+++ src/app/(pages)/create/category/[id]/subCategory/resource/page.tsx
@@ -2,7 +2,7 @@
 export default function Page() {
     return (
         <div>
-
+            page
         </div>
     )
 }
